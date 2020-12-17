8de6158a957dc7ac40412085106f1d891a70f804
Yi Hsiao <hsiaoyi0504@gmail.com>
2020-12-16 23:01:09 -0500

    merge tiff files for other channels

--- PECCU/merge_tiff.py
+++ PECCU/merge_tiff.py
@@ -16,6 +16,50 @@ for well_alphabet in ['A', 'B']:
                 tif.write(img, metadata=None, photometric='minisblack')
                 del img
 
+print('Processing channel 2 ...')
+for well_alphabat in ['A', 'B']:
+    for well_id in range(1, 24):
+        files = sorted(glob('/nfs/turbo/umms-jzsexton/VirtualStaining/BRO0117014VirtStain-20X_20201111_163715/PECCU/PECCU_{}{:02d}_T0001F001L01A0*Z*C02.tif'.format(well_alphabet, well_id)))
+        new_file_name = files[0].split('/')[-1].split('Z01')[0] + 'C02.tif'
+        with tifffile.TiffWriter('/nfs/turbo/umms-jzsexton/VirtualStaining/data_merged/' + new_file_name) as tif:
+            for fname in files:
+                img = tifffile.imread(fname)
+                tif.write(img, metadata=None, photometric='minisblack')
+                del img
+
+print('Processing channel 3 ...')
+for well_alphabat in ['A', 'B']:
+    for well_id in range(1, 24):
+        files = sorted(glob('/nfs/turbo/umms-jzsexton/VirtualStaining/BRO0117014VirtStain-20X_20201111_163715/PECCU/PECCU_{}{:02d}_T0001F001L01A0*Z*C03.tif'.format(well_alphabet, well_id)))
+        new_file_name = files[0].split('/')[-1].split('Z01')[0] + 'C03.tif'
+        with tifffile.TiffWriter('/nfs/turbo/umms-jzsexton/VirtualStaining/data_merged/' + new_file_name) as tif:
+            for fname in files:
+                img = tifffile.imread(fname)
+                tif.write(img, metadata=None, photometric='minisblack')
+                del img
+
+print('Processing channel 4 ...')
+for well_alphabat in ['A', 'B']:
+    for well_id in range(1, 24):
+        files = sorted(glob('/nfs/turbo/umms-jzsexton/VirtualStaining/BRO0117014VirtStain-20X_20201111_163715/PECCU/PECCU_{}{:02d}_T0001F001L01A0*Z*C04.tif'.format(well_alphabet, well_id)))
+        new_file_name = files[0].split('/')[-1].split('Z01')[0] + 'C04.tif'
+        with tifffile.TiffWriter('/nfs/turbo/umms-jzsexton/VirtualStaining/data_merged/' + new_file_name) as tif:
+            for fname in files:
+                img = tifffile.imread(fname)
+                tif.write(img, metadata=None, photometric='minisblack')
+                del img
+
+print('Processing channel 5 ...')
+for well_alphabat in ['A', 'B']:
+    for well_id in range(1, 24):
+        files = sorted(glob('/nfs/turbo/umms-jzsexton/VirtualStaining/BRO0117014VirtStain-20X_20201111_163715/PECCU/PECCU_{}{:02d}_T0001F001L01A0*Z*C05.tif'.format(well_alphabet, well_id)))
+        new_file_name = files[0].split('/')[-1].split('Z01')[0] + 'C05.tif'
+        with tifffile.TiffWriter('/nfs/turbo/umms-jzsexton/VirtualStaining/data_merged/' + new_file_name) as tif:
+            for fname in files:
+                img = tifffile.imread(fname)
+                tif.write(img, metadata=None, photometric='minisblack')
+                del img
+
 # for channel 6
 print('Processing channel 6 (bright field) ...')
 for well_alphabet in ['A', 'B']:
